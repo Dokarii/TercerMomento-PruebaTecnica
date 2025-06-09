649b0c9c66390f652c0f95d032e6ec7609c0f1e1
Dokarii <ricardotejedor18@gmail.com>
2025-06-09 14:46:08 -0500

    Se corrige titulo mal escrito

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -199,7 +199,7 @@ export const DashboardPage = (): JSX.Element => {
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
                 <input
                   type="text"
-                  placeholder="Search subscriptions..."
+                  placeholder="Buscar suscripciones"
                   value={searchTerm}
                   onChange={(e) => setSearchTerm(e.target.value)}
                   className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
